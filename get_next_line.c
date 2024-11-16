9c0972c7c08df094147c29b02f2c46b2e99b67f8
Idriss Mouhammad Outdili <idriss.outdili@gmail.com>
2024-11-17 00:54:05 +0100

    norm + paco

--- get_next_line.c
+++ get_next_line.c
@@ -1,54 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moutdili <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 17:59:24 by moutdili          #+#    #+#             */
+/*   Updated: 2024/11/17 00:48:59 by moutdili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-static char	*extract_line(char **remainder)
+char	*extract_line(char **rem)
 {
-    char	*line;
-    char	*temp;
-    char	*newline;
+	char	*line;
+	char	*temp;
+	char	*newline;
 
-    newline = ft_strchr(*remainder, '\n');
-    if (newline)
-    {
-        line = ft_substr(*remainder, 0, newline - *remainder + 1);
-        temp = ft_strdup(newline + 1);
-        free(*remainder);
-        *remainder = temp;
-    }
-    else
-    {
-        line = ft_strdup(*remainder);
-        free(*remainder);
-        *remainder = NULL;
-    }
-    return (line);
+	newline = ft_strchr(*rem, '\n');
+	if (newline)
+	{
+		line = ft_substr(*rem, 0, newline - *rem + 1);
+		temp = ft_strdup(newline + 1);
+		free(*rem);
+		*rem = temp;
+	}
+	else
+		return (line = ft_strdup(*rem), free(*rem), *rem = NULL, line);
+	return (line);
 }
 
-static char	*read_and_append(int fd, char *remainder)
+void	ft_free(char *remainder, char *buffer)
+{	
+	free(buffer);
+	free(remainder);
+	remainder = NULL;
+}
+
+char	*read_and_append(int fd, char *remainder)
 {
-    char	*buffer;
-    char	*temp;
-    int		read_bytes;
+	char	*buffer;
+	char	*temp;
+	int		read_bytes;
 
-    buffer = malloc(BUFFER_SIZE + 1);
-    if (!buffer)
-        return (NULL);
-
-    while (1)
-    {
-        read_bytes = read(fd, buffer, BUFFER_SIZE);
-        if (read_bytes <= 0)
-            break;
-        buffer[read_bytes] = '\0';
-        if (!remainder)
-            remainder = ft_strdup("");
-        temp = ft_strjoin(remainder, buffer);
-        free(remainder);
-        remainder = temp;
-        if (ft_strchr(buffer, '\n'))
-            break;
-    }
-    free(buffer);
-    return (remainder);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (1)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes <= 0)
+			break ;
+		buffer[read_bytes] = '\0';
+		if (!remainder)
+			remainder = ft_strdup("");
+		temp = ft_strjoin(remainder, buffer);
+		free(remainder);
+		remainder = temp;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	if (read_bytes == -1)
+		return (ft_free(remainder, buffer), NULL);
+	return (free(buffer), remainder);
 }
 
 char	*get_next_line(int fd)
@@ -56,19 +71,12 @@ char	*get_next_line(int fd)
 	static char	*remainder;
 	char		*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	remainder = read_and_append(fd, remainder);
 	if (!remainder)
-        remainder = ft_strdup("");
-    if (fd < 0 || BUFFER_SIZE <= 0 )
-        return (NULL);
-    remainder = read_and_append(fd, remainder);
-    if (!remainder)
-        return (NULL);
-    if (*remainder == '\0')
-    {
-        free(remainder);
-        remainder = NULL;
-        return (NULL);
-    }
-    line = extract_line(&remainder);
-    return (line);
+		return (NULL);
+	if (*remainder == '\0')
+		return (free(remainder), remainder = NULL, NULL);
+	return (line = extract_line(&remainder), line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moutdili <moutdili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 02:29:15 by moutdili          #+#    #+#             */
+/*   Updated: 2024/11/16 19:18:41 by moutdili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,11 +20,14 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+void	ft_free(char *remainder, char *buffer);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*read_and_append(int fd, char *remainder);
+char	*extract_line(char **remainder);
 
 #endif
